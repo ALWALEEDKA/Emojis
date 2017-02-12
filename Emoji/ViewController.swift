a7c80ec5844f0b93b33b2bb8b9f5d6ba4983ef4f
ALWALEED KHALED <akawaleed66@gmail.com>
2017-02-12 22:27:23 +0300

    Initial Commit

---
+++ Emoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji
+//
+//  Created by ALWALEED KHALED on 2/12/17.
+//  Copyright © 2017 KA'S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
